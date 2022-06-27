2ca6ffc01fe7bca2177aa861c54a2f1a9013468b
bigsky77 <simonjudd2@gmail.com>
2022-06-27 07:25:04 -0600

    updated governance contract

--- src/PonyGovernor.sol
+++ src/PonyGovernor.sol
@@ -3,18 +3,17 @@ pragma solidity ^0.8.4;
 
 import "openzeppelin-contracts/contracts/governance/Governor.sol";
 import "openzeppelin-contracts/contracts/governance/extensions/GovernorSettings.sol";
-import "openzeppelin-contracts/contracts/governance/compatibility/GovernorCompatibilityBravo.sol";
+import "openzeppelin-contracts/contracts/governance/extensions/GovernorCountingSimple.sol";
 import "openzeppelin-contracts/contracts/governance/extensions/GovernorVotes.sol";
 import "openzeppelin-contracts/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
-import "openzeppelin-contracts/contracts/governance/extensions/GovernorTimelockControl.sol";
 
-contract PonyGovernor is Governor, GovernorSettings, GovernorCompatibilityBravo, GovernorVotes, GovernorVotesQuorumFraction, GovernorTimelockControl {
-    constructor(IVotes _token, TimelockController _timelock)
+/// @custom:security-contact simon@ponyfinance.xyz
+contract PonyGovernor is Governor, GovernorSettings, GovernorCountingSimple, GovernorVotes, GovernorVotesQuorumFraction {
+    constructor(IVotes _token)
     Governor("PonyGovernor")
-    GovernorSettings(1 /* 1 block */, 13091 /* 1 week */, 0)
+    GovernorSettings(1 /* 1 block */, 13091 /* 2 days */, 1e18)
     GovernorVotes(_token)
     GovernorVotesQuorumFraction(4)
-    GovernorTimelockControl(_timelock)
     {}
 
     // The following functions are overrides required by Solidity.
@@ -46,23 +45,6 @@ contract PonyGovernor is Governor, GovernorSettings, GovernorCompatibilityBravo,
         return super.quorum(blockNumber);
     }
 
-    function state(uint256 proposalId)
-    public
-    view
-    override(Governor, IGovernor, GovernorTimelockControl)
-    returns (ProposalState)
-    {
-        return super.state(proposalId);
-    }
-
-    function propose(address[] memory targets, uint256[] memory values, bytes[] memory calldatas, string memory description)
-    public
-    override(Governor, GovernorCompatibilityBravo, IGovernor)
-    returns (uint256)
-    {
-        return super.propose(targets, values, calldatas, description);
-    }
-
     function proposalThreshold()
     public
     view
@@ -70,38 +52,5 @@ contract PonyGovernor is Governor, GovernorSettings, GovernorCompatibilityBravo,
     returns (uint256)
     {
         return super.proposalThreshold();
-    }
-
-    function _execute(uint256 proposalId, address[] memory targets, uint256[] memory values, bytes[] memory calldatas, bytes32 descriptionHash)
-    internal
-    override(Governor, GovernorTimelockControl)
-    {
-        super._execute(proposalId, targets, values, calldatas, descriptionHash);
-    }
-
-    function _cancel(address[] memory targets, uint256[] memory values, bytes[] memory calldatas, bytes32 descriptionHash)
-    internal
-    override(Governor, GovernorTimelockControl)
-    returns (uint256)
-    {
-        return super._cancel(targets, values, calldatas, descriptionHash);
-    }
-
-    function _executor()
-    internal
-    view
-    override(Governor, GovernorTimelockControl)
-    returns (address)
-    {
-        return super._executor();
-    }
-
-    function supportsInterface(bytes4 interfaceId)
-    public
-    view
-    override(Governor, IERC165, GovernorTimelockControl)
-    returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
     }
 }
